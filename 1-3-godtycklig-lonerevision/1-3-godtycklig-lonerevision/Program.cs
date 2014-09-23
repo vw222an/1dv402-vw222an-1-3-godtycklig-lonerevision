d116d2abd774bbf9ba17863bffb97cba1e28ba2d
vw222an <vw222an@student.lnu.se>
2014-09-23 10:51:58 +0200

    first commit: added some code.

---
+++ 1-3-godtycklig-lonerevision/1-3-godtycklig-lonerevision/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1_3_godtycklig_lonerevision
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfSalaries = 0;
+            Console.Write("Ange antal löner att mata in: ");
+                numberOfSalaries = int.Parse(Console.ReadLine());
+
+            if (numberOfSalaries < 2)
+            {
+                Console.BackgroundColor = ConsoleColor.Red;
+                Console.WriteLine("Du måste mata in minst två löner för att kunna göra en beräkning!");
+                Console.ResetColor();
+
+                if (Console.ReadKey(true).Key != ConsoleKey.Escape)
+                {
+                    Environment.Exit(0);
+                }
+
+                else
+                {
+
+                }
+                
+            }
+
+            
+
+        }
+
+        static void ProcessSalaries(int numberOfSalaries)
+        {
+            int[] salaries = new int[numberOfSalaries];
+        }
+    }
+}
